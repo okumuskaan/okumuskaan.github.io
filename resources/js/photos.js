2a5b7e25afdcc0af49c63744bac578edfca6f5f8
okumuskaan <okumuskaan@hotmail.com>
2022-10-18 19:05:41 +0200

    Update photo-more-info

--- resources/js/photos.js
+++ resources/js/photos.js
@@ -7,13 +7,19 @@ let hide_showButton_icon = hide_showButton.getElementsByClassName("open-close-to
 let arrows = [toolbar.getElementsByClassName("arrows left")[0],
               toolbar.getElementsByClassName("arrows right")[0]];
 
+let infoBox = document.getElementById("shown-photo-more-info-container")
+let infoList = Array.from(document.getElementsByClassName("photo-title-more-info"));
+let infoTitle = document.getElementById("more-info-title");
+let infoCloseBtn = document.getElementById("closebtn-more-info");
+
 let shownPhotoInfoContainer = document.getElementById("shown-photo-info-container");
 let shownPhotoInfoImg = document.getElementById("info-img");
 
 const photo_names = ["prague", "beyoglu", "stopwar", "underground", "swanlake"];
 const photo_titles = ["Prague Castle", "Beyoglu'nda Insaat Calismasi",
-                      "Stop War!", "Yeralti", "Swan Lake in Lausanne"];
-const photo_dates = ["2019", "2019", "2019", "2019", "2021"];
+                      "Stop War!", "Yeralti", "Swan Lake at Lausanne"];
+const photo_dates = ["July, 2019", "May, 2019", "July, 2019", "July, 2019",
+                     "December, 2021"];
 const photo_places = ["Prague, Czechia", "Istanbul, Turkey", "Berlin, Germany",
                       "Ankara, Turkey", "Lausanne, Switzerland"];
 let photo_infos = {}
@@ -26,7 +32,6 @@ for (i=0; i<photo_names.length; i++) {
 }
 
 let photos_data = {};
-
 photo_names.forEach((name) => {
     let box = document.getElementById(name);
     photos_data[name] = {
@@ -36,12 +41,21 @@ photo_names.forEach((name) => {
 });
 let chosen_photo_name = ["prague"];
 
+
+function update_photo_infos(chosen_photo_name) {
+    shownPhotoTitle.innerHTML = photo_infos[chosen_photo_name]["title"];
+    infoTitle.innerHTML = photo_infos[chosen_photo_name]["title"];
+    infoList[1].innerHTML = "Date: " + photo_infos[chosen_photo_name]["date"];
+    infoList[2].innerHTML = "Place: " + photo_infos[chosen_photo_name]["place"];
+    console.log("SUCCESSFULLY UPDATED");
+}
+
 function on_click_photo(evt) {
     shownPhoto.src = photos_data[evt.target.id]["src"];
     photos_data[chosen_photo_name]["html-img"].setAttribute("class", "photos not-chosen");
     chosen_photo_name = evt.target.id;
     photos_data[chosen_photo_name]["html-img"].setAttribute("class", "photos chosen-down");
-    shownPhotoTitle.innerHTML = photo_infos[chosen_photo_name]["title"];
+    update_photo_infos(chosen_photo_name);
 }
 
 let ind_open_close = 0;
@@ -93,10 +107,9 @@ function on_mouseout_show_info() {
     shownPhotoInfoImg.style.opacity = "0.7";
 }
 
-let infoBox = document.getElementById("shown-photo-more-info-container")
-let infoList = Array.from(document.getElementsByClassName("photo-title-more-info"));
-let infoTitle = document.getElementById("more-info-title");
-let infoCloseBtn = document.getElementById("closebtn-more-info");
+
+
+
 
 function on_click_show_info() {
 
@@ -114,6 +127,20 @@ function on_click_show_info() {
     infoTitle.style.fontSize = "16px";
 
 }
+function on_click_infoCloseBtn() {
+    infoBox.style.height = "0%";
+    infoBox.style.width = "0%";
+    infoBox.style.padding = "0px";
+    infoBox.style.border = "none";
+    infoList.forEach((info) => {
+        info.style.fontSize = "0px";
+    });
+    infoCloseBtn.style.width = "0px";
+    infoCloseBtn.style.padding = "0px";
+    infoCloseBtn.style.fontSize =  "0px";
+    infoTitle.style.borderBottom = "none";
+    infoTitle.style.fontSize = "0px";
+}
 
 photo_names.forEach((name) => {
     photos_data[name]["html-img"].addEventListener("click", on_click_photo);
@@ -123,20 +150,5 @@ shownPhotoInfoContainer.addEventListener("mouseover", on_mouseover_show_info);
 shownPhotoInfoContainer.addEventListener("mouseout", on_mouseout_show_info);
 shownPhotoInfoContainer.addEventListener("click", on_click_show_info);
 
-function on_click_infoCloseBtn() {
-    console.log("CloseInfoBtn Clicked!!")
-        infoBox.style.height = "0%";
-        infoBox.style.width = "0%";
-        infoBox.style.padding = "0px";
-        infoBox.style.border = "none";
-        infoList.forEach((info) => {
-            info.style.fontSize = "0px";
-        });
-        infoCloseBtn.style.width = "0px";
-        infoCloseBtn.style.padding = "0px";
-        infoCloseBtn.style.fontSize =  "0px";
-        infoTitle.style.borderBottom = "none";
-        infoTitle.style.fontSize = "0px";
-}
 
 infoCloseBtn.addEventListener("click", on_click_infoCloseBtn);
